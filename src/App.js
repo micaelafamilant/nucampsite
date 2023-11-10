6c682051c94edb62a408f101b89525f88f83e693
Micaela <mmlfamilant@gmail.com>
2023-11-09 20:36:59 -0500

    Campsites Directory Page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import CampsitesList from './features/campsites/CampsitesList';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <CampsitesList />
+      <CampsitesDirectoryPage />
       <Footer />
     </div>
   );
