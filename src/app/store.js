1b011e6975c5cde5282a382fc045f330af36bd51
Micaela <mmlfamilant@gmail.com>
2024-03-25 20:58:20 -0400

    Redux slices and selectors

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { campsiteReducer } from '../features/campsites/campsitesSlice';
+import { promotionsReducer } from '../features/promotions/promotionsSlice';
+import { partnersReducer } from '../features/partners/partnersSlice';
+import { commentsReducer } from '../features/comments/commentsSlice'; 
 
 export const store = configureStore({
   reducer: {
     campsites: campsiteReducer,
+    promotions: promotionsReducer,
+    partners: partnersReducer,
+    comments: commentsReducer
   },
 });
--- src/features/comments/commentsSlice.js
+++ src/features/comments/commentsSlice.js
@@ -1,7 +1,20 @@
 import { COMMENTS } from "../../app/shared/COMMENTS";
+import { createSlice } from "@reduxjs/toolkit";
 
-export const selectCommentsByCampsiteId = (campsiteId) => {
-    return COMMENTS.filter(
+const initialState = {
+    commentsArray: COMMENTS
+};
+
+const commentsSlice = createSlice({
+    name: "comments",
+    initialState
+});
+
+export const commentsReducer = commentsSlice.reducer;
+//this is a method on the object returned by the createSlice() function. It is the reducer function that will be used to update the state in the Redux store.
+
+export const selectCommentsByCampsiteId = (campsiteId) => (state) => {
+    return state.comments.commentsArray.filter(
         (comment) => comment.campsiteId === parseInt(campsiteId)
     );
 };
