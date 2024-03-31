a88b9d11e4a346965c5131948d76a00fab399d41
Micaela <mmlfamilant@gmail.com>
2024-03-31 14:38:35 -0400

    add comment is working

--- src/features/comments/commentsSlice.js
+++ src/features/comments/commentsSlice.js
@@ -16,12 +16,18 @@ const commentsSlice = createSlice({
                 id: state.commentsArray.length + 1,
                 ...action.payload
             }
+            state.commentsArray.push(newComment);
         }
     }
 });
 
 export const commentsReducer = commentsSlice.reducer;
-//this is a method on the object returned by the createSlice() function. It is the reducer function that will be used to update the state in the Redux store.
+//this is a method on the object returned by the createSlice() function. 
+//It is the reducer function that will be used to update the state in the Redux store.
+
+export const { addComment } = commentsSlice.actions;
+//This takes the addComment action creator from the commentsSlice object and exports it as a named export. 
+//This allows us to import it into other files and use it to create actions.
 
 export const selectCommentsByCampsiteId = (campsiteId) => (state) => {
     return state.comments.commentsArray.filter(
